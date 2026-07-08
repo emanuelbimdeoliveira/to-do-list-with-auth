a962ee6c65b4c5eb606610a296e905402d5b147c
EMANUEL DOS SANTOS BIM DE OLIVEIRA <130716978+EmanuelBimDeOliveira@users.noreply.github.com>
2026-07-08 20:38:57 -0300

    chore: initial structure of files added

---
+++ back-end/routes/userRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.post("/register");
+// router.get("/me");
+// router.get("/profile/:id");
+// router.post("/login");
+
+export default router;
